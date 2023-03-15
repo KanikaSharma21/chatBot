72ae7f57179adebda2c2954f9377c03da24b6462
Kanika <kanika.sharma@qyuki.com>
2023-03-15 12:46:16 +0530

    add chat completion api with gpt-3.5-turbo

--- server/server.js
+++ server/server.js
@@ -21,17 +21,36 @@ app.get("/", async (req, res) => {
   });
 });
 
+// app.post("/", async (req, res) => {
+//   try {
+//     const prompt = req.body.prompt;
+    
+//     const response = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: `${prompt}`,
+//       temperature: 0,
+//       max_tokens: 3000,
+//       top_p: 1,
+//       frequency_penalty: 0.5,
+//       presence_penalty: 0,
+//     });
+//     // console.log(response.data)
+//     res.status(200).send({
+//         bot:response.data.choices[0].text
+//     })
+//   } catch (error) {
+//     console.log(error.response.data.error);
+//     res.status(500).send({error})
+//   }
+// });
+
 app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
+    
+    const response = openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: `${prompt}`}],
     });
     // console.log(response.data)
     res.status(200).send({
